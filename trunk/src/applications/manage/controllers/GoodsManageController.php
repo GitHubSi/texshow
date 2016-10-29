bd868f28134443e317191c64a91b34f7a0810a9f
GitHubSi <2493393471@qq.com>
2016-10-29 21:41:21 +0800

    goods management

--- trunk/src/applications/manage/controllers/GoodsManageController.php
+++ trunk/src/applications/manage/controllers/GoodsManageController.php
@@ -32,8 +32,68 @@ class GoodsManageController extends AbstractSecurityAction
         $this->_smarty->display('admin/b-index.tpl');
     }
 
-    public function addGoodAction()
+    public function modifyAction()
     {
+        $name = $this->getParam("name");
+        $totalScore = $this->getParam("price");
+        $image = $this->getParam("image");
+        $desc = $this->getParam("desc");
+        $type = $this->getParam("type");
+        $itemId = $this->getParam("id");
 
+        if (empty($name) || empty($totalScore) || empty($image) || empty($desc)) {
+            header("Location: /GoodsManage/index?successMsg=参数不能为空");
+            return;
+        }
+        if ($type == "edit" && !ctype_digit($itemId)) {
+            header("Location: /GoodsManage/index?errMsg=");
+            return;
+        }
+
+        //just for extension in the future
+        $descArray = array(
+            array(
+                "title" => "",
+                "image" => $desc
+            )
+        );
+
+        $insertDay = date("Y-m-d H:i:s");
+        if ($type == "add") {
+            $this->_shareItemMapper->insertGood($name, $image, json_encode($descArray), $totalScore, $insertDay, $insertDay);
+        }
+        if ($type == "edit") {
+            $this->_shareItemMapper->updateGood($itemId, $name, $image, json_encode($descArray), $totalScore, $insertDay, $insertDay);
+        }
+
+        header("Location: /GoodsManage/index?successMsg=添加成功");
     }
+
+    public function infoAction()
+    {
+        $type = $this->getParam("type");
+        $itemId = $this->getParam("id");
+
+        $allowType = array(
+            "detail", "edit", "add"
+        );
+        if (!in_array($type, $allowType)) {
+            header("Location: /GoodsManage/index?successMsg=类型参数错误");
+            return;
+        }
+
+        if (!empty($itemId)) {
+            $goodsInfo = $this->_shareItemMapper->getGoodById($itemId);
+
+            $descArr = json_decode($goodsInfo["desc"], true);
+            $goodsInfo["desc"] = $descArr[0]["image"];
+        }
+
+        $this->_smarty->assign("good", $goodsInfo);
+        $this->_smarty->assign('id', $itemId);
+        $this->_smarty->assign('type', $type);
+        $this->_smarty->assign('tpl', "admin/good_manage/add.tpl");
+        $this->_smarty->display('admin/b-index.tpl');
+    }
+
 }
